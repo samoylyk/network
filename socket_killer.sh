4408a8e5c0ea67552aee53c1c1b6a79707500944
Alexander Samoylyk <alexander@samoylyk.net>
2020-06-02 16:28:14 +0200

    added socket_killer.sh

---
+++ socket_killer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PID=$(pidof /usr/bin/atserver 2>/dev/null)
+TIMEOUT="7200" # seconds
+
+if [[ -z "$PID" ]]; then
+	exit
+fi
+
+while read -r line; do
+        fd=$(echo "$line" | sed -r 's/[[:alnum:]]+=/\n&/g' | awk -F= '$1=="fd"{print $2}' | awk '{print $1}' | sed 's/[^0-9]*//g')
+        last_activity=$(echo "$line" | sed -r 's/[[:alnum:]]+:/\n&/g' | awk -F: '$1=="lastsnd"{print $2}' | sed 's/[^0-9]*//g')
+
+        if [[ $last_activity -ge $((TIMEOUT*1000)) ]]; then
+                gdb -p "$PID" --batch -ex "call close($fd)" -ex quit
+        fi
+done < <(ss -t -a -p -n -i | awk '/atserver/{printf $0 ;next;}1' | grep "pid=$PID," | grep ESTAB)
