3d5a479f0ef936b8373edc21d5757160539cf349
Alexander Samoylyk <alexander@samoylyk.net>
2020-06-02 16:30:15 +0200

    updated socket_killer.sh

--- socket_killer.sh
+++ socket_killer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PID=$(pidof /usr/bin/atserver 2>/dev/null)
+PROC="some_proc"
+PID=$(pidof -s $PROC 2>/dev/null)
 TIMEOUT="7200" # seconds
 
 if [[ -z "$PID" ]]; then
@@ -8,10 +9,10 @@ if [[ -z "$PID" ]]; then
 fi
 
 while read -r line; do
-        fd=$(echo "$line" | sed -r 's/[[:alnum:]]+=/\n&/g' | awk -F= '$1=="fd"{print $2}' | awk '{print $1}' | sed 's/[^0-9]*//g')
-        last_activity=$(echo "$line" | sed -r 's/[[:alnum:]]+:/\n&/g' | awk -F: '$1=="lastsnd"{print $2}' | sed 's/[^0-9]*//g')
+	fd=$(echo "$line" | sed -r 's/[[:alnum:]]+=/\n&/g' | awk -F= '$1=="fd"{print $2}' | awk '{print $1}' | sed 's/[^0-9]*//g')
+	last_activity=$(echo "$line" | sed -r 's/[[:alnum:]]+:/\n&/g' | awk -F: '$1=="lastsnd"{print $2}' | sed 's/[^0-9]*//g')
 
-        if [[ $last_activity -ge $((TIMEOUT*1000)) ]]; then
-                gdb -p "$PID" --batch -ex "call close($fd)" -ex quit
-        fi
-done < <(ss -t -a -p -n -i | awk '/atserver/{printf $0 ;next;}1' | grep "pid=$PID," | grep ESTAB)
+	if [[ $last_activity -ge $((TIMEOUT*1000)) ]]; then
+		gdb -p "$PID" --batch -ex "call close($fd)" -ex quit
+	fi
+done < <(ss -t -a -p -n -i | awk '/'"$PROC"'/{printf $0 ;next;}1' | grep "pid=$PID," | grep ESTAB)
